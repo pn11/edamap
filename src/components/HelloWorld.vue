6395c348a1099ac73de445f27c88254b504666fd
oka <pn11@users.noreply.github.com>
2024-07-15 02:19:36 +0900

    Minimum working example of leaflet

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="map" style="height: 100vh;"></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import * as L from 'leaflet';
+
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+});
+</script>
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100%;
+}
+</style>
