8d55dd4b38c6dbf72c00d55fff27591f17a32924
oka <pn11@users.noreply.github.com>
2024-07-15 17:51:40 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import './style.css'
 import App from './App.vue'
 import 'leaflet/dist/leaflet.css'
 import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
+import "leaflet/dist/images/marker-icon-2x.png";
+import "leaflet/dist/images/marker-shadow.png";
 
 createApp(App).mount('#app')
